63ac4a7ea3c90f9ab89a5ce973fa87facd42daab
khouloud <rouisskhouloud@gmail.com>
2025-12-23 20:50:52 +0100

    fix: prevent password field from being serialized

--- src/main/java/com/khouloud/auth/model/User.java
+++ src/main/java/com/khouloud/auth/model/User.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -22,6 +24,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Builder
 @AllArgsConstructor
@@ -40,7 +43,9 @@ public class User implements UserDetails {
 	
 	@Column(nullable = false, unique = true)
 	private String email;
-
+	
+	@JsonIgnore
+	@ToString.Exclude
 	private String password;
 
 	private Date createdAt;
